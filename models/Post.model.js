1e95cbce38c4a560d1483131466c987e224fdc43
ana luz martinez <almadigital.mx@gmail.com>
2022-07-16 02:53:05 -0700

    post model updated

--- models/Post.model.js
+++ models/Post.model.js
@@ -1,32 +1,23 @@
-// const { Schema, model } = require("mongoose");
+const { Schema, model } = require("mongoose");
 
-// const postSchema = new Schema(
-//   {
-//       _author: 
-//        {type: mongoose.Schema.Types.ObjectId,
-//        ref: 'User'
-//       },
-//       copy: {
-//        type: String,
-//        trim: true,
-//        required: true
-//      },
-//       date: {
-//        type: Date,
-//        default: Date.now
-//      },
-//       comments: [{
-//        type: mongoose.Schema.Types.ObjectId,
-//        ref: 'Comment'
-//       }]
-//   },
-//     {
-//     timestamps: true
-//     }
-// );
+const postSchema = new Schema(
+  {
+    _author: { type: Schema.Types.ObjectId, ref: "User" },
+    comment: String,
+          date: {
+       type: Date,
+       default: Date.now
+     },
+    _organization : {type: Schema.Types.ObjectId, ref: "Organization" },
+    _country: {type: Schema.Types.ObjectId, ref: "Country" },
+  },
+  {
+    timestamps: true
+  }
+);
 
+//nota como commen requiere el id del user, entonces jalas con ref el user.
 
+const Post = model("Post", postSchema);
 
-// const Post = model("Post", postSchema);
-
-// module.exports = Post;
+module.exports = Post;
